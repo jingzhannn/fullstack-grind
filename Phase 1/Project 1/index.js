6f4722a54d503b04b2451bef515bcdb5c4e9421d
NPJingZhan <s10266800@connect.np.edu.sg>
2026-03-19 16:21:38 +0800

    Node + Express Basics (GET Only)

---
+++ Phase 1/Project 1/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/intro', (req, res) => {
+  res.send("Im Jing Zhan. I want to go SMU");
+})
+
+app.get('/name', (req, res) => {
+  res.send("Sew Jing Zhan");
+})
+
+app.get('/hobbies', (req, res) => {
+  const hobbies = ["coding", "reading", "cycling"];
+  
+  res.json(hobbies);
+});
+
+app.get('/food', (req, res) => {
+  res.send("Chicken");
+})
+
+app.get('/student', (req, res) => {
+  const profile = {
+    "name" : "Jing Zhan",
+    "hobbies" : ["coding", "reading", "cycling"],
+    "University" : "Singapore Management University"
+  }
+
+  res.json(profile);
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+}); 
